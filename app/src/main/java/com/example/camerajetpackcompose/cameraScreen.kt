dc7d0e3fb018e2f28ec0c7ca3ee2f7c59ee309c3
PQ-Thinh <thinh.pq.64cntt@ntu.edu.vn>
2025-07-24 17:20:37 +0700

    update Button Camera

--- app/src/main/java/com/example/camerajetpackcompose/cameraScreen.kt
+++ app/src/main/java/com/example/camerajetpackcompose/cameraScreen.kt
@@ -6,14 +6,33 @@ import androidx.camera.core.ImageCapture
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.compose.LocalLifecycleOwner
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
 
+
+@androidx.compose.ui.tooling.preview.Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun CameraScreen(){
 
@@ -31,6 +50,40 @@ fun CameraScreen(){
 
     val imageCapture = remember {
         ImageCapture.Builder().build()
+    }
+    LaunchedEffect(Unit) {
+        val cameraProvider = context.getCameraProvider()
+
+        cameraProvider.unbindAll()
+
+        cameraProvider.bindToLifecycle(
+            lifecycleOwner = lifecycleOwner,
+            cameraSelector = cameraSelector,
+            preview,
+            imageCapture
+        )
+        preview.setSurfaceProvider(previewView.surfaceProvider)
+    }
+
+    Box(modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.BottomCenter
+    ) {
+        AndroidView(factory = {previewView}, modifier = Modifier.fillMaxSize())
+
+        Box(
+            modifier = Modifier.fillMaxWidth()
+                .background(Color.Black.copy(alpha = 0.7f))
+                .padding(16.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            IconButton(onClick = {},
+                modifier = Modifier.size(50.dp)
+                    .background(color = Color.White, CircleShape)
+                    .padding(8.dp)
+                    .background(color = Color.Red, CircleShape)
+                  ) {
+            }
+        }
     }
 }
 
